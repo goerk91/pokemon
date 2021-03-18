20079c28688a83a1f3d173446d333b175e69ce67
Metin G <goerken91@gmx.de>
2021-03-18 23:09:02 +0100

    test comment

--- src/features/Pagination.js
+++ src/features/Pagination.js
@@ -3,8 +3,11 @@ import React from 'react'
 export default function Pagination({ goToNextPage, goToPrevPage }) {
     return (
         <div>
+
             {goToPrevPage && <button onClick={goToPrevPage}>Previous</button>}
             <button onClick={goToNextPage}>Next</button>
         </div>
     )
 }
+
+//test
